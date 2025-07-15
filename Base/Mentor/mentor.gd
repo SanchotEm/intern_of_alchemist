ab90f9933b061006b4bcf7e399d90a0fe9e40256
Static-Conection-Studios <ppcarciofi@gmail.com>
2025-07-14 23:32:57 -0300

    Thought I was done for today but ended up coding a bit more, oops!

--- Base/Mentor/mentor.gd
+++ Base/Mentor/mentor.gd
@@ -8,7 +8,8 @@ var state_chances :Array[int] = [20, 20, 0, 0, 0]
 #MOVING = travelling across the screen, therefore not interactable
 #LINGERING = On the screen, in the way, halfway transparent
  #Goes away/flies arround again on it's own after a few seconds
-#LINGERING_CLICK = will get gradualy more transparent and moves arround as you click on it, eventuray disapearing
+#LINGERING_CLICK = will get gradualy more transparent and moves arround as you click on it,
+ #eventuray disapearing
 #LINGERING_WAVE = idem, but or waving the mouse on its face
 #LINGERING_WAVE_ITEM = idem, but waving an item (ingredient) on its face
 #LINGERING_GRAB_ITEM = idem, but wants you to grab a specific item it's hovering above of
@@ -29,6 +30,9 @@ var linger_amount_range = Vector2i(0, 3) #min and max amount of time mentor will
 var i_t_per_click :int = 8 #increase transparency per click
 
 
+func _ready() -> void:
+	move_to(Vector2(500, 500))
+
 
 func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event.is_action("LMB"):
@@ -37,19 +41,55 @@ func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int
 				pass
 
 
-func show_up() -> void:
+func show_up() -> void: #Appears in a random position, then enters a LINGERING state
 	
-	move_to_random_position(true) #will allways show up on a random position
+	await move_to_random_position(true) #will allways show up on a random position
 	#Might move to speciic position later, such as if it rolls LINGERING_GRAB_ITEM
 	show()
 	modulate = Color(1, 1, 1, 1)
 	
 	say_random()
 	
-	
+	var sum :int = 0
+	for c in state_chances:
+		sum += c
+	var i :int = randi_range(1, sum)
+	if i <= state_chances[0]:
+		pass
+	elif i <= state_chances[1]:
+		pass
+	elif i <= state_chances[2]:
+		pass
+	elif i <= state_chances[3]:
+		pass
+	elif i <= state_chances[4]:
+		pass
+	#else:
+		#else:
+		#print("'show_up' on mentor didn't work propery. i = ", str(i))
+
 
 func move_to_random_position(instant :bool = false) -> void: #Mentor moves to a random position
-	pass
+	
+	var siz_w :Vector2 = get_window().size
+	var siz_s :Vector2 = Vector2($Sprite.texture.get_width(), $Sprite.texture.get_height()) * $Sprite.scale
+	var pos = Vector2(randf_range(siz_s.x/2, siz_w.x - siz_s.x/2), randf_range(siz_s.y/2, siz_w.y - siz_s.y/2))
+	
+	if instant:
+		position = pos
+	else:
+		await move_to(pos)
+
+func move_to(pos :Vector2) -> void: #Moves toward pos with an animation
+	
+	var starting_state :MentorStates = mentor_state
+	mentor_state = MentorStates.MOVING
+	
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "position", pos, 1).set_trans(Tween.TRANS_SINE)
+	await tween.finished
+	
+	mentor_state = starting_state
 
 func say_random() -> void:
 	

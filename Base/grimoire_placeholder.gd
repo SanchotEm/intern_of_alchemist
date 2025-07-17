062b03d252a48f53706c8c60c45e0eb8627815f0
Subnitrates <ch2435@gmail.com>
2025-07-17 10:30:29 -0500

    stuff

--- Base/grimoire_placeholder.gd
+++ Base/grimoire_placeholder.gd
@@ -6,3 +6,4 @@ extends Sprite2D
 func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event.is_action_pressed("LMB"):
 		SignalBus.grimoire_clicked.emit()
+		# Opens the Grimoire, a UI element, currently UI/ScrollUIControl

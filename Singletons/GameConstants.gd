3138e798069d1e0712244c8934c214eabc19d728
subnitrates <subnitrates@gmail.com>
2025-07-17 20:34:03 -0500

    Test (#39)
    * fixed up resource loader for audio system
    
    * mentor enhancements
    
    movement speech compatible
    
    ---------
    
    Co-authored-by: Subnitrates <ch2435@gmail.com>

--- Singletons/GameConstants.gd
+++ Singletons/GameConstants.gd
@@ -5,7 +5,7 @@ extends Node
 const MIN_VOLUME_VALUE: int = 1
 const MAX_VOLUME_VALUE: int = 100
 
-const DEFAULT_MAIN_VOLUME: int = 50
+const DEFAULT_MAIN_VOLUME: int = 25
 const DEFAULT_MUSIC_VOLUME: int = 50
 const DEFAULT_SFX_VOLUME: int = 50
 const DEFAULT_NARRATOR_VOLUME: int = 50

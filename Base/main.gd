7937a908b294b50cac83c7e0ae8bb6de2aa263bf
SanchotEm <danila_stupidu@mail.ru>
2025-07-19 11:26:44 +0700

    Reapply "Stable patch"
    This reverts commit 073cd41674be0d681de3175011246e03e335a5a3.

--- Base/main.gd
+++ Base/main.gd
@@ -10,6 +10,7 @@ func _process(delta: float) -> void:
 	_game_cycle()
 
 func _ready() -> void:
+	TranslationServer.set_locale("en")
 	Interface.register_player(Interface.AudioPlayerType.SFX, %SFXPlayer)
 	Interface.register_player(Interface.AudioPlayerType.MUSIC, %MusicPlayer)
 
